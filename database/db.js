e3b1ec8dfe4714ba36bc36b2ef5e0342a6470513
Amarmanhar <58129626+Amarmanhar@users.noreply.github.com>
2023-11-07 23:33:56 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dbUrl = "mongodb+srv://nkamar1412:Amar9669@cluster0.xnnoefu.mongodb.net/";
+const dbUrl = "mydb";
 
 const MongoDb = mongoose.connect(dbUrl).then(()=>{
     console.log(`databse is connected`);
